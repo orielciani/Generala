4872c74e7bec76653e56a45735ea7a6dc4024e20
orielciani <otrielciani@gmail.com>
2020-03-17 20:12:14 -0300

    html, css modificados, funciones nada.

---
+++ www/js/my-app.js
@@ -0,0 +1,48 @@
+var $$ = Dom7;
+
+var app = new Framework7({
+  root: '#app', // App root element
+
+  id: 'io.framework7.myapp', // App bundle ID
+  name: 'My App', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+
+
+  // Cordova Statusbar settings
+  statusbar: {
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+  },
+  
+
+});
+
+
+var mainView = app.views.create('.view-main');
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+    // Do something here when page loaded and initialized
+    console.log(e);
+})
+
+// Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    console.log(e);
+    alert('Hello');
+})
