e93153acc1a0bf09bc4b0bfa27a1c2ac470e0556
Lucas_Diodati <lucasmartindiodati@gmail.com>
2020-03-23 19:25:17 -0300

    se muestra el 1 cuando se edita

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,24 +1,16 @@
 var $$ = Dom7;
-
 var app = new Framework7({
   root: '#app', // App root element
-
   id: 'io.framework7.myapp', // App bundle ID
   name: 'My App', // App name
   theme: 'auto', // Automatic theme detection
-  // App root data
-  // App root methods
   methods: {
     helloWorld: function () {
       app.dialog.alert('Hello World!');
     },
-    cambiarValor: function () {
-      // funcion global para cambiar el valor clickeado
-    },
   },
   // App routes
   routes: routes,
-
   // Cordova Statusbar settings
   statusbar: {
     iosOverlaysWebView: true,
@@ -27,24 +19,6 @@ var app = new Framework7({
 });
 
 var mainView = app.views.create('.view-main');
-   /* var dados1 = [1, 2, 3, 4, 5, "-", "X"];
-    var dados2 = [2, 4, 6, 8, 10, "-", "X"];
-    var dados3 = [3, 6, 9, 12, 15, "-", "X"];
-    var dados4 = [4, 8, 12, 16, 20, "-", "X"];
-    var dados5 = [5, 10, 15, 20, 25, "-", "X"];
-    var dados6 = [6, 12, 18, 24, 30, "-", "X"];*/
-   // var dadosN = [1, 2, 3, 4, 5, "-", "X"];
-/*    var dadosEscalera = [20, 25, "-", "X"];
-    var dadosFull = [30, 35, "-", "X"];
-    var dadosPoker = [40, 45, "-", "X"];
-    var dadosGenerala = [50, 55, "-", "X"];
-    var dadosDoblegenerala = [100, 105, "-", "X"];*/
- //   var dadosJuego = ["Servido", "No servido", "-", "X"];
-
-var i = 0;
-// DOM events for About popover
-//$$('.popover-popover2').on('popover:open', function (e) {  });
-//$$('.popover-popover2').on('popover:opened', function (e) {  });
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
@@ -56,91 +30,7 @@ $$(document).on('deviceready', function() {
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
     console.log(e);
-   /* $$('.open-vertical').on('click', function () {
-  app.dialog.create({
-    title: 'Puntajes: ',
-    buttons: [
-      {
-        text: '1',
-      },
-      {
-        text: '2',
-      },
-      {
-        text: '3',
-      },
-      {
-        text: '4',
-      },
-      {
-        text: '5',
-      },
-      {
-        text: '-',
-      },
-      {
-        text: 'X',
-      },
-    ],
-    onClick: function (dialog, index) {
-            if(index === 0){
-              $$("#d_1_1").text(dados1[0]);
-            }
-            else if(index === 1){
-                //Button 2 clicked
-                $$("#d_1_1").text(dados1[1]);
-            }
-            else if(index === 2){
-                //Button 3 clicked
-                $$("#d_1_1").text(dados1[2]);
-            }
-            else if(index === 3){
-                //Button 3 clicked
-                $$("#d_1_1").text(dados1[3]);
-            }
-            else if(index === 4){
-                //Button 3 clicked
-                $$("#d_1_1").text(dados1[4]);
-            }
-            else if(index === 5){
-                //Button 3 clicked
-                $$("#d_1_1").text(dados1[5]);
-            }
-             else if(index === 6){
-                //Button 3 clicked
-                $$("#d_1_1").text(dados1[6]);
-            }
-        },
-    verticalButtons: true,
-  }).open();
-});*/
 })
-// aca termina el Dialog
-
-/*
-
-$$("#popover1").on("click", function() {popover(0)});
-$$("#popover2").on("click", function() {popover(1)});
-$$("#popover3").on("click", function() {popover(2)});
-$$("#popover4").on("click", function() {popover(3)});
-$$("#popover5").on("click", function() {popover(4)});
-$$("#popover6").on("click", function() {popover(5)});
-$$("#popover7").on("click", function() {popover(6)});
-function popover(n) {
-  $$("#d_2_1").text(dados2[n])
-}
-// aca termina el popover1
-$$("#1popover").on("click", function() {popover2(0)});
-$$("#2popover").on("click", function() {popover2(1)});
-$$("#3popover").on("click", function() {popover2(2)});
-$$("#4popover").on("click", function() {popover2(3)});
-$$("#5popover").on("click", function() {popover2(4)});
-$$("#6popover").on("click", function() {popover2(5)});
-$$("#7popover").on("click", function() {popover2(6)});
-function popover2(n) {
-  $$("#d_3_1").text(dados3[n])
-}
-*/
 
 $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
 // asigne clase valor a todas las celdas que pueden tener valor asignable, asigno a var local el id del tag capturado
@@ -153,24 +43,30 @@ $$(document).on('page:init', '.page[data-name="anotador"]', function (e) {
 
     // capturar el boton seleccionado del popover
     var popSeleccion = "";
-$$('#popover1').on('click',function(){popSeleccion = "1"; console.log(popSeleccion); app.popover.close();});
-$$('#popover2').on('click',function(){popSeleccion = "2"; console.log(popSeleccion); app.popover.close();});
-$$('#popover3').on('click',function(){popSeleccion = "3"; console.log(popSeleccion); app.popover.close();});
-$$('#popover4').on('click',function(){popSeleccion = "4"; console.log(popSeleccion); app.popover.close();});
-$$('#popover5').on('click',function(){popSeleccion = "5"; console.log(popSeleccion); app.popover.close();});
-$$('#popover6').on('click',function(){popSeleccion = "-"; console.log(popSeleccion); app.popover.close();});
-$$('#popover7').on('click',function(){popSeleccion = "X"; console.log(popSeleccion); app.popover.close();});
+$$('#popover1').on('click',function(){popSeleccion = "1"; console.log(popSeleccion); cambiarValor("1"); app.popover.close();});
+$$('#popover2').on('click',function(){popSeleccion = "2"; console.log(popSeleccion); cambiarValor("2"); app.popover.close();});
+$$('#popover3').on('click',function(){popSeleccion = "3"; console.log(popSeleccion); cambiarValor("3"); app.popover.close();});
+$$('#popover4').on('click',function(){popSeleccion = "4"; console.log(popSeleccion); cambiarValor("4"); app.popover.close();});
+$$('#popover5').on('click',function(){popSeleccion = "5"; console.log(popSeleccion); cambiarValor("5"); app.popover.close();});
+$$('#popover6').on('click',function(){popSeleccion = "-"; console.log(popSeleccion); cambiarValor("-"); app.popover.close();});
+$$('#popover7').on('click',function(){popSeleccion = "X"; console.log(popSeleccion); cambiarValor("X"); app.popover.close();});
 
-$$('#servido').on('click',function(){popSeleccion = "servido"; console.log(popSeleccion); app.popover.close();});
-$$('#noServido').on('click',function(){popSeleccion = "no servido"; console.log(popSeleccion); app.popover.close();});
-$$('#juegoCancela').on('click',function(){popSeleccion = "-"; console.log(popSeleccion); app.popover.close();});
-$$('#juegoAnula').on('click',function(){popSeleccion = "X"; console.log(popSeleccion); app.popover.close();});
+$$('#servido').on('click',function(){popSeleccion = "servido"; console.log(popSeleccion); cambiarValor("s");  app.popover.close();});
+$$('#noServido').on('click',function(){popSeleccion = "no servido"; console.log(popSeleccion); cambiarValor("ns");  app.popover.close();});
+$$('#juegoCancela').on('click',function(){popSeleccion = "-"; console.log(popSeleccion); cambiarValor("-");  app.popover.close();});
+$$('#juegoAnula').on('click',function(){popSeleccion = "X"; console.log(popSeleccion); cambiarValor("X");  app.popover.close();});
+var seleccionado ="";
+function cambiarValor(n) {
+      // funcion global para cambiar el valor clickeado
+$$('#'+seleccionado).text(n);
 
 
+
+    };
 
 
 $$('.valor').on('click',function(){
-var seleccionado = $$(this).attr('id');
+seleccionado = $$(this).attr('id');
 console.log(seleccionado);
     var partes = seleccionado.split("_");
 
@@ -181,7 +77,14 @@ console.log(seleccionado);
 console.log("celda"+" - "+p1+" jugador "+p2);
 
 // calcular el valor que se pone segun la celda seleccionada
+var valorCelda = "";
+if(p1 == "1"){
+valorCelda == popSeleccion;
+}else if(p1 == "2"){
+  valorCelda == parseInt(popSeleccion)*2;
+}
 
+$$('#'+seleccionado).text(valorCelda);
 
 //asignarlo a la celda del id asignado a "seleccionado"
 
